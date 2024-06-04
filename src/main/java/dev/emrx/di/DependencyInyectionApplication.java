765b71e7f585b889f79539f8f2e4c9e10601d2cb
Elvis M <u201112959@upc.edu.pe>
2024-06-04 17:15:31 -0500

    Inyeccion de dependencia por atributo con spring

--- src/main/java/dev/emrx/di/DependencyInyectionApplication.java
+++ src/main/java/dev/emrx/di/DependencyInyectionApplication.java
@@ -1,13 +1,21 @@
 package dev.emrx.di;
 
+import dev.emrx.di.atributo.Coche;
+import dev.emrx.di.atributo.Motor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DependencyInyectionApplication.class, args);
-	}
+    public static void main(String[] args) {
+//        Motor motor = new Motor("Xl1", 1981);
+//        Coche coche = new Coche("VW", 1986, motor);
+        ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
+        Coche coche = context.getBean(Coche.class);
+
+        System.out.println(coche);
+    }
 
 }
