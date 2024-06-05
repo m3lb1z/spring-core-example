b1f95188b344fc912287dbe851da9ba41eed29e2
Elvis M <u201112959@upc.edu.pe>
2024-06-04 21:14:10 -0500

    definiendo beans de forma explicita

--- src/main/java/dev/emrx/di/DependencyInyectionApplication.java
+++ src/main/java/dev/emrx/di/DependencyInyectionApplication.java
@@ -10,22 +10,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
 
     private static final Logger log = LoggerFactory.getLogger(DependencyInyectionApplication.class);
 
-    private Animal animal;
+    // definiendo beans de forma explicita
+    @Bean
+    public String getApplicationName() {
+        return "Spring Core learning";
+    }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
-        EjemploScopeService ejemploScopeService1 = context.getBean(EjemploScopeService.class);
-        EjemploScopeService ejemploScopeService2 = context.getBean(EjemploScopeService.class);
+        String nombreDeAplicacion = context.getBean(String.class);
 
-        log.info("Are beans equal {}", ejemploScopeService1.equals(ejemploScopeService2));
-        log.info("Are beans == {}", ejemploScopeService1 == ejemploScopeService2);
-
+        log.info("Nombre de la aplicación: {}", nombreDeAplicacion);
     }
 
 }
